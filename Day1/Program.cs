a07994196cd1b26fcfef30e3683a858de34a5da1
Michal Baše <m.base@quadient.com>
2022-12-08 11:53:53 +0100

    initial tasks

---
+++ Day1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+var lines = await File.ReadAllLinesAsync("..\\..\\..\\input1.txt");
+Console.WriteLine("Input:");
+
+foreach (var line in lines)
+{
+    Console.WriteLine(line);
+}
